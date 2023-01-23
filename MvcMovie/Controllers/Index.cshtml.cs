19bc3ae5061e3c097422e78f444595866df26917
PatPal <PatPal@DESKTOP-1LNTL0S>
2023-01-23 23:14:43 +0200

    Add project files.

---
+++ MvcMovie/Controllers/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MvcMovie.Data.MvcMovieContext _context;
+
+        public IndexModel(MvcMovie.Data.MvcMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
