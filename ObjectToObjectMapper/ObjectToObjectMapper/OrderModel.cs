d48e69e08f5a19501af3df30ec756157df1aef61
Patrick Lapimaa <113598320+PatrickLapik@users.noreply.github.com>
2023-05-24 11:36:06 +0300

    Add files via upload

---
+++ ObjectToObjectMapper/ObjectToObjectMapper/OrderModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectToObjectMapper
+{
+    internal class OrderModel
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string DeliveryAddress { get; set; }
+        public DateTime EstimatedDeliveryDate { get; set; }
+        public string OrderReference { get; set; }
+    }
+}
